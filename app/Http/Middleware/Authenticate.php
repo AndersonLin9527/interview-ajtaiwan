41c131cabdfd5dc1a4b3067fa999b12b1b94f11e
Anderson <henxo9527@gmail.com>
2021-04-11 21:06:47 +0800

    UPDATE Laravel Auth unauthenticated redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,16 +6,20 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
-    }
+  /**
+   * Get the path the user should be redirected to when they are not authenticated.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @return string|null
+   * @author Anderson 2021-04-11
+   */
+  protected function redirectTo($request)
+  {
+//    Laravel Default
+//    if (!$request->expectsJson()) {
+//      return route('login');
+//    }
+    // 此段移到 app/Exceptions/Handler::unauthenticated() 集中處理
+    return null;
+  }
 }

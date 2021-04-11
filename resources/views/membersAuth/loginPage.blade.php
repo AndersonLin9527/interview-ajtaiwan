72dcf633ee1bf5ac042aefdbc0388fa32371ce8c
Anderson <henxo9527@gmail.com>
2021-04-11 22:50:35 +0800

    UPDATE View loginPage, registerPage (beautify)

--- resources/views/membersAuth/loginPage.blade.php
+++ resources/views/membersAuth/loginPage.blade.php
@@ -2,51 +2,37 @@
 @section('htmlHeadTitle','登入頁 - '.config('env.APP_NAME'))
 @section('htmlHeadStyle')
   <style>
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: auto;
-    }
+    /*.form-signin {*/
+    /*  width: 100%;*/
+    /*  max-width: 330px;*/
+    /*  padding: 15px;*/
+    /*  margin: auto;*/
+    /*}*/
 
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
+    /*.form-signin .checkbox {*/
+    /*  font-weight: 400;*/
+    /*}*/
 
-    .form-signin .form-floating:focus-within {
-      z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
+    /*.form-signin .form-floating:focus-within {*/
+    /*  z-index: 2;*/
+    /*}*/
   </style>
 @endsection
-@section('htmlBodyClass','text-center')
 @section('htmlBody')
   <main class="form-signin">
     <form action="<?=route('membersAuth.login')?>" enctype="multipart/form-data" method="post">
-      <h1 class="h3 mb-4 fw-normal">會員登入</h1>
-
+      <h1 class="h3 mb-4 fw-normal text-center text-primary">會員登入</h1>
+      {{-- 帳號 --}}
       <div class="form-floating mb-3">
-        <input name="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com"
-               required>
-        <label for="floatingInput">帳號</label>
+        <input name="username" type="text" class="form-control" id="username" placeholder="帳號" required>
+        <label for="username">帳號</label>
       </div>
-      <div class="form-floating">
-        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password"
-               required>
-        <label for="floatingPassword">密碼</label>
+      {{-- 密碼 --}}
+      <div class="form-floating mb-3">
+        <input name="password" type="password" class="form-control" id="password" placeholder="Password" required>
+        <label for="password">密碼</label>
       </div>
-
+      {{-- 記住我 & 會員註冊--}}
       <div class="row mb-3 g-3 align-items-center">
         <div class="col-6 text-start">
           <div class="checkbox text-start">
@@ -61,14 +47,12 @@
           </a>
         </div>
       </div>
-
       <button class="w-100 btn btn-lg btn-primary" type="submit">登入</button>
       @csrf
-
       <?php
-      dump(session()->all());
+//      dump(session()->all());
       ?>
     </form>
-    <p class="mt-5 mb-3 text-muted">&copy; <?=date('Y')?></p>
+    <p class="mt-5 mb-3 text-center text-muted">&copy; <?=date('Y')?></p>
   </main>
 @endsection
--- resources/views/membersAuth/registerPage.blade.php
+++ resources/views/membersAuth/registerPage.blade.php
@@ -1,69 +1,89 @@
 @extends('_templates.extends.extends_membersAuth')
 @section('htmlHeadTitle','註冊頁 - '.config('env.APP_NAME'))
-@section('htmlHeadStyle')
-  <style>
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: auto;
-    }
-
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
-
-    .form-signin .form-floating:focus-within {
-      z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-  </style>
-@endsection
-@section('htmlBodyClass','text-center')
 @section('htmlBody')
   <main class="form-signin">
-    <form action="<?=route('membersAuth.login')?>" enctype="multipart/form-data" method="post">
-      <h1 class="h3 mb-4 fw-normal">會員註冊</h1>
-
-      <div class="form-floating">
-        <input name="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">帳號</label>
+    <form action="<?=route('membersAuth.register')?>" enctype="multipart/form-data" method="post">
+      <h1 class="h3 mb-4 fw-normal text-center text-success">會員註冊</h1>
+      {{-- 帳號 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="username">帳號 <span class="text-danger">*</span></label>
+        <div class="col-sm-9">
+          <input class="form-control" id="username" name="username" required type="text">
+        </div>
       </div>
-
-      <div class="form-floating">
-        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">密碼</label>
+      {{-- 密碼 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="password">密碼 <span class="text-danger">*</span></label>
+        <div class="col-sm-9">
+          <input class="form-control" id="password" name="password" required type="password">
+        </div>
       </div>
-
-      <div class="form-floating">
-        <input name="password_confirm" type="password" class="form-control" id="floatingPasswordConfirm"
-               placeholder="Password">
-        <label for="floatingPasswordConfirm">密碼確認</label>
+      {{-- 密碼確認 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="password_confirm">密碼確認 <span class="text-danger">*</span></label>
+        <div class="col-sm-9">
+          <input class="form-control" id="password" name="password_confirm" required type="password">
+        </div>
       </div>
-
-      <div class="checkbox mb-3">
-        <label>
-          <input name="remember_me" type="checkbox" value="1"> 記住我
-        </label>
+      {{-- 性別 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="sex">性別</label>
+        <div class="col-sm-9 pt-2">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex" id="inlineRadio1" value="0">
+            <label class="form-check-label" for="inlineRadio1">女</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex" id="inlineRadio2" value="1">
+            <label class="form-check-label" for="inlineRadio2">男</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex" id="inlineRadio3" value="2" disabled>
+            <label class="form-check-label" for="inlineRadio3">公司</label>
+          </div>
+        </div>
       </div>
-
+      {{-- 生日 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="sex">生日</label>
+        <div class="col-sm-9">
+          <div class="input-group">
+            <input type="number" class="form-control" name="birthday_year" maxlength="4" placeholder="西元"
+                   aria-label="Birthday Year">
+            <span class="input-group-text">年</span>
+            <select class="form-select form-select-sm" name="birthday_month" aria-label="Birthday Month">
+              @foreach(range(1,12) as $month)
+                <option value="{{str_pad($month,2,'0',STR_PAD_LEFT )}}">{{$month}}</option>
+              @endforeach
+            </select>
+            <span class="input-group-text">/</span>
+            <select class="form-select form-select-sm" name="birthday_day" aria-label="Birthday Day">
+              @foreach(range(1,30) as $day)
+                <option value="{{str_pad($day,2,'0',STR_PAD_LEFT )}}">{{$day}}</option>
+              @endforeach
+            </select>
+          </div>
+        </div>
+      </div>
+      {{-- 信箱 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="username">信箱</label>
+        <div class="col-sm-9">
+          <input class="form-control" id="username" name="username" placeholder="abc@def.com" type="email">
+        </div>
+      </div>
+      {{-- 會員登入 --}}
+      <div class="row mb-3 g-3 align-items-center">
+        <div class="col text-end">
+          <a class="text-primary" href="<?=route('membersAuth.loginPage')?>">
+            會員登入
+          </a>
+        </div>
+      </div>
+      {{-- 註冊 --}}
       <button class="w-100 btn btn-lg btn-success" type="submit">註冊</button>
       @csrf
-      <?php
-      //      dump(session()->all());
-      ?>
     </form>
-    <p class="mt-5 mb-3 text-muted">&copy; <?=date('Y')?></p>
+    <p class="mt-5 mb-3 text-muted text-center">&copy; <?=date('Y')?></p>
   </main>
 @endsection

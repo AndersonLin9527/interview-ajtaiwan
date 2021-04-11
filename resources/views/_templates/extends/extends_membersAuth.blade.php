b6a8a3bd4524de4c11c041998390944b824ce258
Anderson <henxo9527@gmail.com>
2021-04-12 02:36:10 +0800

    IMPORT jQuery 3.3.1 & UPDATE view extends_membersAuth.blade.php

--- resources/views/_templates/extends/extends_membersAuth.blade.php
+++ resources/views/_templates/extends/extends_membersAuth.blade.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('htmlHeadTitle',config('env.APP_NAME'))</title>
-
   {{-- Bootstrap CSS --}}
   <link href="<?=asset('plugin/bootstrap/5.0.0-beta3/css/bootstrap.min.css');?>" rel="stylesheet">
   {{-- Bootstrap bundle JS --}}
   <script src="<?=asset('plugin/bootstrap/5.0.0-beta3/js/bootstrap.bundle.min.js');?>"></script>
-
+  {{-- jQuery 3.3.1 --}}
+  <script src="<?=asset('plugin/jquery/3.3.1/jquery.min.js');?>"></script>
   <link href="<?=asset('css/style_membersAuth.css');?>" rel="stylesheet">
   @yield('htmlHeadStyle')
 </head>

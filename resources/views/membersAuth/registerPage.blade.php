3faee34c8290b0a3782055f39af892f465accd5f
Anderson <henxo9527@gmail.com>
2021-04-12 02:41:16 +0800

    UPDATE view registerPage.blade.php | old() & validateErrorMessages

--- resources/views/membersAuth/registerPage.blade.php
+++ resources/views/membersAuth/registerPage.blade.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @var Illuminate\Support\ViewErrorBag $errors
+ */
+?>
 @extends('_templates.extends.extends_membersAuth')
 @section('htmlHeadTitle','註冊頁 - '.config('env.APP_NAME'))
 @section('htmlBody')
@@ -8,21 +13,32 @@
       <div class="mb-2 row">
         <label class="col-sm-3 col-form-label" for="username">帳號 <span class="text-danger">*</span></label>
         <div class="col-sm-9">
-          <input class="form-control" id="username" name="username" required type="text">
+          <input class="form-control" id="username" maxlength="50" minlength="5" name="username" required type="text"
+                 value="<?=old('username');?>">
         </div>
       </div>
       {{-- 密碼 --}}
       <div class="mb-2 row">
         <label class="col-sm-3 col-form-label" for="password">密碼 <span class="text-danger">*</span></label>
         <div class="col-sm-9">
-          <input class="form-control" id="password" name="password" required type="password">
+          <input class="form-control" id="password" minlength="5" name="password" required type="password"
+                 value="<?=old('password');?>">
         </div>
       </div>
       {{-- 密碼確認 --}}
       <div class="mb-2 row">
         <label class="col-sm-3 col-form-label" for="password_confirm">密碼確認 <span class="text-danger">*</span></label>
         <div class="col-sm-9">
-          <input class="form-control" id="password" name="password_confirm" required type="password">
+          <input class="form-control" id="password_confirm" minlength="5" name="password_confirm" required
+                 type="password" value="<?=old('password_confirm');?>">
+        </div>
+      </div>
+      {{-- 姓名 --}}
+      <div class="mb-2 row">
+        <label class="col-sm-3 col-form-label" for="name">姓名 <span class="text-danger">*</span></label>
+        <div class="col-sm-9">
+          <input class="form-control" id="name" maxlength="50" name="name" required type="text"
+                 value="<?=old('name');?>">
         </div>
       </div>
       {{-- 性別 --}}
@@ -42,36 +58,68 @@
             <label class="form-check-label" for="inlineRadio3">公司</label>
           </div>
         </div>
+        @if(!is_null(old('sex')))
+          <script>
+            $('input[name="sex"][value="<?=old('sex');?>"]').prop("checked", true);
+          </script>
+        @endif
       </div>
       {{-- 生日 --}}
       <div class="mb-2 row">
         <label class="col-sm-3 col-form-label" for="sex">生日</label>
         <div class="col-sm-9">
           <div class="input-group">
-            <input type="number" class="form-control" name="birthday_year" maxlength="4" placeholder="西元"
-                   aria-label="Birthday Year">
-            <span class="input-group-text">年</span>
+            <input aria-label="Birthday Year" class="form-control" maxlength="4" name="birthday_year"
+                   onkeyup="value=value.replace(/[^\d]/g,'')" pattern="\d*" placeholder="西元" type="text"
+                   value="<?=old('birthday_year');?>">
+            <span class="input-group-text"></span>
             <select class="form-select form-select-sm" name="birthday_month" aria-label="Birthday Month">
+              <option value="">- 請選擇 -</option>
               @foreach(range(1,12) as $month)
                 <option value="{{str_pad($month,2,'0',STR_PAD_LEFT )}}">{{$month}}</option>
               @endforeach
             </select>
+            @if(!is_null(old('birthday_month')))
+              <script>
+                $('select[name="birthday_month"] option[value="<?=old('birthday_month');?>"]').prop("selected", true);
+              </script>
+            @endif
             <span class="input-group-text">/</span>
             <select class="form-select form-select-sm" name="birthday_day" aria-label="Birthday Day">
+              <option value="">- 請選擇 -</option>
               @foreach(range(1,30) as $day)
                 <option value="{{str_pad($day,2,'0',STR_PAD_LEFT )}}">{{$day}}</option>
               @endforeach
             </select>
+            @if(!is_null(old('birthday_day')))
+              <script>
+                $('select[name="birthday_day"] option[value="<?=old('birthday_day');?>"]').prop("selected", true);
+              </script>
+            @endif
           </div>
         </div>
       </div>
       {{-- 信箱 --}}
       <div class="mb-2 row">
-        <label class="col-sm-3 col-form-label" for="username">信箱</label>
+        <label class="col-sm-3 col-form-label" for="email">信箱</label>
         <div class="col-sm-9">
-          <input class="form-control" id="username" name="username" placeholder="abc@def.com" type="email">
+          <input class="form-control" id="email" name="email" placeholder="abc@def.com" type="email"
+                 value="<?=old('email');?>">
         </div>
       </div>
+      {{-- 錯誤訊息 --}}
+      @if($errors->isNotEmpty())
+        <div class="mb-2 row">
+          <label class="col-sm-3 col-form-label text-danger pt-0">錯誤訊息</label>
+          <div class="col-sm-9">
+            @foreach ($errors->getMessages() as $messages)
+              @foreach ($messages as $message)
+                <span class="d-block mb-1 text-danger">※ {{$message}}</span>
+              @endforeach
+            @endforeach
+          </div>
+        </div>
+      @endif
       {{-- 會員登入 --}}
       <div class="row mb-3 g-3 align-items-center">
         <div class="col text-end">

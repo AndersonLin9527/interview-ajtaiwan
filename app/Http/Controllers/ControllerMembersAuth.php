53d501768093d1dacb0c3432ad7374eec6cf3071
Anderson <henxo9527@gmail.com>
2021-04-12 02:39:56 +0800

    UPDATE ControllerMembersAuth.php | register with validation

--- app/Http/Controllers/ControllerMembersAuth.php
+++ app/Http/Controllers/ControllerMembersAuth.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ControllerMembersAuth extends Controller
 {
@@ -90,11 +91,79 @@ class ControllerMembersAuth extends Controller
   /**
    * 會員註冊
    *
+   * @param Request $request
+   * @return RedirectResponse
    * @author Anderson 2021-04-11
    */
-  public function register()
+  public function register(Request $request): RedirectResponse
   {
-    return 'register';
+    $username = $request->input('username');
+    $password = $request->input('password');
+    $name = $request->input('name');
+    $sex = $request->input('sex');
+    $birthday = $request->input('birthday_year') . '-' . $request->input('birthday_month') . '-' . $request->input('birthday_day');
+    $email = $request->input('email');
+
+    $validateData = [
+      'username' => $username,
+      'password' => $password,
+      'password_confirm' => $request->input('password_confirm'),
+      'name' => $name,
+      'sex' => $sex,
+      'birthday' => $birthday,
+      'email' => $email,
+    ];
+
+    $validateRules = [
+      'username' => ['required', 'string', 'max:50', 'min:5', 'unique:members,username',],
+      'password' => ['required', 'string', 'min:5'],
+      'password_confirm' => ['required', 'string', 'min:5', 'same:password'],
+      'name' => ['required', 'string', 'max:50'],
+      'sex' => ['nullable', 'in:0,1'],
+      'birthday' => ['nullable', 'date_format:Y-m-d'],
+      'email' => ['nullable', 'email'],
+    ];
+
+    $validateMessages = [
+      'username.required' => '帳號是必填的',
+      'username.max' => '帳號最多為 50 個字',
+      'username.min' => '帳號最少需 5 個字',
+      'username.unique' => '帳號 ' . $username . ' 已被使用',
+      'password.required' => '密碼是必填的',
+      'password.min' => '密碼最少需 5 個字',
+      'password_confirm.min' => '密碼確認最少需 5 個字',
+      'password_confirm.same' => '密碼與確認密碼不相符',
+      'name.required' => '姓名是必填的',
+      'name.max' => '姓名最多為 50 個字',
+      'sex.in' => '性別不合法',
+      'birthday.date_format' => '生日格式錯誤',
+      'email.email' => '信箱格式錯誤',
+    ];
+
+    // 資料驗證
+    $Validator = Validator::make($validateData, $validateRules, $validateMessages);
+
+    // 資料驗證驗證失敗時
+    if (!$Validator->passes()) {
+      return redirect()->back()->withInput($request->all())->withErrors($Validator->getMessageBag());
+    }
+
+    $Members = new Members();
+
+    $Members->create([
+      'username' => $username,
+      'password' => bcrypt($password),
+      'name' => $name,
+      'sex' => $sex,
+      'birthday' => $birthday,
+      'email' => $email,
+      'remember_token' => null,
+      'last_login_at' => null,
+      'last_login_ip' => null,
+    ]);
+
+    session()->flash('message', 'registerSuccess');
+    return redirect()->route('membersAuth.loginPage');
   }
 
 }

a9244d2c4b67ceb1f2f79803a21fcd0fb28b9f2e
Anderson <henxo9527@gmail.com>
2021-04-12 20:08:25 +0800

    UPDATE config google.php | site_key & secret_key can update at .env

--- config/google.php
+++ config/google.php
@@ -4,8 +4,8 @@ return [
     // input name
     'input_name' => 'googleReCaptchaV3Token',
     // 網站金鑰
-    'site_key' => '6LdLfaYaAAAAAFgSO801myd2B352GJQ_mUGnVBeH',
+    'site_key' => env('GOOGLE_RECAPTCHA_V3_SITE_KEY', '6LdLfaYaAAAAAFgSO801myd2B352GJQ_mUGnVBeH'),
     // 網站密鑰
-    'secret_key' => '6LdLfaYaAAAAAPfNkrkYDwXyvWNa4Lfd-matj1nt',
+    'secret_key' => env('GOOGLE_RECAPTCHA_V3_SECRET_KEY', '6LdLfaYaAAAAAPfNkrkYDwXyvWNa4Lfd-matj1nt'),
   ],
 ];
